7301dec8549b6878375ed2ce6051f9cec6748706
Marius Storhaug <marstor@hotmail.com>
2024-04-10 00:59:18 +0200

    test

--- test/run.ps1
+++ test/run.ps1
@@ -1,7 +1,17 @@
 using namespace System.Net
 
 # Input bindings are passed in via param block.
-param($Request, $TriggerMetadata)
+param(
+    # HttpRequestContext object containing the request data.
+    [Parameter()]
+    [HttpRequestContext] $Request,
+
+    # Trigger metadata object containing the metadata for the trigger.
+    [Parameter()]
+    $TriggerMetadata
+)
+
+Write-Host "TriggerMetadata is of type '$($TriggerMetadata.GetType().FullName)'."
 
 Write-Host 'PowerShell HTTP trigger function processed a request.'
 
