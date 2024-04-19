9ae9218a7b4f8d08e5de83a6ed3e9d3e70baaf54
Marius Storhaug <marstor@hotmail.com>
2024-04-19 22:24:33 +0200

    test

--- test/run.ps1
+++ test/run.ps1
@@ -66,7 +66,7 @@ switch ($githubEvent) {
 ### $githubEvent
 [$user]($($Request.Body.sender.html_url)) just $action a $githubEvent on [$repo]($($Request.Body.repository.html_url))
 "@
-            embeds  = $null
+            embeds  = @{}
         }
     }
 }
