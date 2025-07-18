eb28b6168ca1423aeee219d1b4eb3f6b2ac5759a
Marius Storhaug <marstor@hotmail.com>
2025-07-18 13:51:21 +0200

    Enhance logging in main.ps1 by adding type and member information for Request and TriggerMetadata; adjust formatting for environment variables and GitHub context output.

--- app/info/main.ps1
+++ app/info/main.ps1
@@ -9,22 +9,26 @@ param(
 )
 
 Write-Information 'Request'
+Write-Information "$($Request.GetType())"
+Write-Information "$($Request | Get-Member)"
 Write-Information "$($Request | ConvertTo-Json -Depth 10)"
 
 Write-Information 'TriggerMetadata'
+Write-Information "$($TriggerMetadata.GetType())"
+Write-Information "$($TriggerMetadata | Get-Member)"
 Write-Information "$($TriggerMetadata | ConvertTo-Json -Depth 10)"
 
 Write-Information 'PSVersionTable'
 Write-Information "$($PSVersionTable | Out-String)"
 
 Write-Information 'Environment variables'
-Write-Information "$(Get-ChildItem -Path Env: | Format-Table -Property Name, Value | Out-String)"
+Write-Information "$(Get-ChildItem -Path Env: | Format-Table -Property Name, Value -AutoSize | Out-String)"
 
 Write-Information 'AzContext'
 Write-Information "$(Get-AzContext | Format-List | Out-String)"
 
 Write-Information 'GitHubContext'
-Write-Information "$(Get-GitHubContext | Format-List | Out-String)"
+Write-Information "$(Get-GitHubContext | Select-Object * | Format-List | Out-String)"
 
 Write-Information 'GitHubConfig'
 Write-Information "$(Get-GitHubConfig | Format-List | Out-String)"
