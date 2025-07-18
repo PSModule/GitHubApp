9e7d0922fa2af196a5e1cce3e2f51423c85f531b
Marius Storhaug <marstor@hotmail.com>
2025-07-18 13:07:26 +0200

    Fix Azure account connection by adding AccountId parameter to Connect-AzAccount

--- app/profile.ps1
+++ app/profile.ps1
@@ -1,4 +1,4 @@
 $null = Disable-AzContextAutosave -Scope Process
-Connect-AzAccount -Identity
+Connect-AzAccount -Identity -AccountId $env:AZURE_CLIENT_ID
 
 # Connect-GitHub -ClientID 'Iv23liYDnEbKlS9IVzHf' -KeyVaultKeyReference 'https://psmodule-test-vault.vault.azure.net/keys/psmodule-org-app'
