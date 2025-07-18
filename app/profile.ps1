5b80a74421d904f0fba5255cff3b8a64137d7cf4
Marius Storhaug <marstor@hotmail.com>
2025-07-18 12:34:02 +0200

    Refactor Azure context connection logic by removing conditional check for MSI_SECRET

--- app/profile.ps1
+++ app/profile.ps1
@@ -1,6 +1,4 @@
-if ($env:MSI_SECRET) {
-    $null = Disable-AzContextAutosave -Scope Process
-    Connect-AzAccount -Identity
-}
+$null = Disable-AzContextAutosave -Scope Process
+Connect-AzAccount -Identity
 
 # Connect-GitHub -ClientID 'Iv23liYDnEbKlS9IVzHf' -KeyVaultKeyReference 'https://psmodule-test-vault.vault.azure.net/keys/psmodule-org-app'
