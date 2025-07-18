9b6ff9adf14ccc2e114e3d9080d3a13f9f9a7005
Marius Storhaug <marstor@hotmail.com>
2025-07-18 14:33:17 +0200

    Add HTTP trigger support and response handling in main.ps1; create function.json for webhook configuration

--- app/info/main.ps1
+++ app/info/main.ps1
@@ -1,9 +1,12 @@
 using namespace System.Net
 
 param(
+    # The HTTP request context
     [Parameter()]
     [HttpRequestContext] $Request,
 
+    # Metadata about the trigger
+    # This can include information like the function name, invocation ID, etc.
     [Parameter()]
     [hashtable] $TriggerMetadata
 )
@@ -23,10 +26,31 @@ Get-ChildItem -Path Env: | ForEach-Object { $envHash[$_.Name] = $_.Value }
 Write-Information "$([PSCustomObject]$envHash | Format-List | Out-String)"
 
 Write-Information 'AzContext'
-Write-Information "$(Get-AzContext | Format-List | Out-String)"
+Write-Information "$(Get-AzContext -DefaultProfile | Format-List | Out-String)"
+
+Write-Information 'AzConfig'
+Write-Information "$(Get-AzConfig | Format-List | Out-String)"
 
 Write-Information 'GitHubContext'
 Write-Information "$(Get-GitHubContext | Select-Object * | Format-List | Out-String)"
 
 Write-Information 'GitHubConfig'
 Write-Information "$(Get-GitHubConfig | Format-List | Out-String)"
+
+Push-OutputBinding -Name Response -Value (
+    [HttpResponseContext]@{
+        StatusCode  = [int][HttpStatusCode]::UnsupportedMediaType
+        ContentType = 'application/json'
+        Headers     = @{
+            'WhatEver' = 'You want to put here'
+        }
+        Body        = [pscustomobject]@{
+            Message        = 'Content type is not application/json.'
+            AzContext      = Get-AzContext
+            GitHubContext  = Get-GitHubContext
+            GitHubConfig   = Get-GitHubConfig
+            PSVersionTable = $PSVersionTable
+            Environment    = $envHash
+        }
+    }
+)
